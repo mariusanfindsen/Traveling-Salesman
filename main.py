89b27b6bc031cd4f073e8b1e25f827126d68d478
mariusanfindsen <marius@maksimer.no>
2021-10-20 18:36:35 +0200

    Set city data when creating new instance of TravelingSalesperson

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 class TravelingSalesperson:
-    def __init__(self, population_size, generations, mutationRate, init_population, cities):
-        self.mutationRate, self.generations = mutationRate, generations
+    def __init__(self, population_size, generations, mutation_rate, init_population, cities):
+        self.mutation_rate, self.generations = mutation_rate, generations
         self.population_size, self.best_route, self.init_population = population_size, None, init_population
+
+        with open('european_cities.csv') as infile:  # Open's CSV file with data
+            # Create´s a list of the cities
+            self.cities = infile.readline().split(';')[:cities]
+            # Create´s a dict where the city is the key and the city´s index is it´s value
+            self.cities_keys = {self.cities[i]: i for i in range(len(self.cities))}
+            # Storing the distances between the cities
+            self.distances = [distance.split(';')[:len(self.cities)] for distance in infile.readlines()]
+        infile.close()
 
     def init_population(self, population_size):
         pass
